8e1eac0c8f72c71f4e5d665b499766807b141660
monupal1122 <monupalji1122@gmail.com>
2026-02-17 13:27:21 +0530

    fix: add HelmetProvider to prevent crash on page reload

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import { Toaster as Sonner } from "./components/ui/sonner";
 import { TooltipProvider } from "./components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HelmetProvider } from 'react-helmet-async';
 import Index from "./pages/Index";
 import ArticleDetail from "./pages/ArticleDetail";
 import CategoryPage from "./pages/CategoryPage";
@@ -17,26 +18,28 @@ import ScrollToTop from "./components/utils/ScrollToTop";
 const queryClient = new QueryClient();
 
 const App = () => (
-    <QueryClientProvider client={queryClient}>
-        <TooltipProvider>
-            <Toaster />
-            <Sonner />
-            <BrowserRouter>
-                <ScrollToTop />
-                <Routes>
-                    <Route path="/" element={<Index />} />
-                    <Route path="/:category/:subcategory/:slugId" element={<ArticleDetail />} />
-                    <Route path="/category/:category" element={<CategoryPage />} />
-                    <Route path="/subcategory/:category/:subcategory" element={<SubcategoryPage />} />
-                    <Route path="/author/:id" element={<AuthorProfile />} />
-                    <Route path="/tags/:tag" element={<TagPage />} />
-                    <Route path="/search" element={<SearchPage />} />
-                    {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
-                    <Route path="*" element={<NotFound />} />
-                </Routes>
-            </BrowserRouter>
-        </TooltipProvider>
-    </QueryClientProvider>
+    <HelmetProvider>
+        <QueryClientProvider client={queryClient}>
+            <TooltipProvider>
+                <Toaster />
+                <Sonner />
+                <BrowserRouter>
+                    <ScrollToTop />
+                    <Routes>
+                        <Route path="/" element={<Index />} />
+                        <Route path="/:category/:subcategory/:slugId" element={<ArticleDetail />} />
+                        <Route path="/category/:category" element={<CategoryPage />} />
+                        <Route path="/subcategory/:category/:subcategory" element={<SubcategoryPage />} />
+                        <Route path="/author/:id" element={<AuthorProfile />} />
+                        <Route path="/tags/:tag" element={<TagPage />} />
+                        <Route path="/search" element={<SearchPage />} />
+                        {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
+                        <Route path="*" element={<NotFound />} />
+                    </Routes>
+                </BrowserRouter>
+            </TooltipProvider>
+        </QueryClientProvider>
+    </HelmetProvider>
 );
 
 export default App;
